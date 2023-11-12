21f1252eb936d5c9a6044fef1e7f14f184648bea
estevo <pedro.cordova2002@gmail.com>
2023-11-12 20:00:30 -0300

    query selectors add

--- newjs/js/app.js
+++ newjs/js/app.js
@@ -46,8 +46,49 @@ produtos.filter(item => item.preco > 200).map(item => {
     return item
 })
 
-const h1 = document.querySelector('h1');
-console.log(h1)
+// criando link
+let link = document.createElement('a');
+link.setAttribute('href', '#');
 
-console.log(h1.innerText)
-console.log(h1.classList.value.split(' '))
+let content = document.createTextNode('Clique Aqui para testar');
+
+link.appendChild(content);
+// fim da criação do link
+
+let containerClass = document.querySelector('#container');
+let container = document.querySelector('.container');
+
+container.appendChild(link);
+
+// alterar o css por meio dos selectors
+link.style.color = '#ffff';
+link.style.padding = '36px';
+link.style.textDecoration = 'none';
+console.log(link.style)
+
+// ver o tamanho dos elementos
+console.log(container.clientHeight)
+console.log(container.clientWidth)
+console.log(container.offsetLeft)
+console.log(container.clientTop)
+
+
+// ver a distancia do elemento para o top e left
+let box = document.querySelector('.box');
+console.log(box.offsetTop)
+console.log(box.offsetLeft)
+
+// ver o tamanho da tela de modo geral
+console.log(window.innerWidth)
+console.log(window.innerHeight)
+
+// ver a rolagem da pagina
+console.log(window.scrollY)
+console.log(window.scrollX)
+
+// const h1 = document.querySelector('h1');
+// console.log(h1)
+
+// ver o que tem dentro do elemento selecionado
+// console.log(h1.innerText)
+// console.log(h1.classList.value.split(' '))
