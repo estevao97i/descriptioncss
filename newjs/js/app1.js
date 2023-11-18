0576fe38a2fe0c8745b52e41028b88b05c055736
estevo <pedro.cordova2002@gmail.com>
2023-11-18 13:13:24 -0300

    modal add

--- newjs/js/app1.js
+++ newjs/js/app1.js
@@ -1,5 +1,8 @@
 document.querySelectorAll('.accordion-content').forEach(elem => {
     elem.style.maxHeight = '0'
+
+    // const btnModal = elem.nextElementSibling
+    // console.log(btnModal)
 })
 
 const buttons = document.querySelectorAll('.accordion-button').forEach( button => {
@@ -7,14 +10,30 @@ const buttons = document.querySelectorAll('.accordion-button').forEach( button =
     button.addEventListener('click', () => {
 
         const content = button.nextElementSibling
-        console.log(content)
         
         button.classList.toggle('active')
-
+        console.log(button)
+        
         if (button.classList.contains('active')) {
             content.style.maxHeight = content.scrollHeight + 'px';
         } else {
             content.style.maxHeight = '0'
         }
+        
+        if (content.querySelector('button')) {
+            const btnModal = content.querySelector('button')
+            
+            btnModal.addEventListener('click', () => {
+                
+                const modal = content.querySelector('.modal');
+                modal.classList.add('modal-active')
+
+                const close = modal.querySelector('.close');
+                close.addEventListener('click', () => {
+                    modal.classList.remove('modal-active')
+                })
+            })
+        }
     })
 })
+
