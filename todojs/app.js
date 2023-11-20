813279cf49a75b4bdb326fff1be734e9d2868176
estevo <pedro.cordova2002@gmail.com>
2023-11-20 09:35:16 -0300

    finished todolist js

--- todojs/app.js
+++ todojs/app.js
@@ -2,55 +2,70 @@ let list = document.querySelector(".list");
 let input = document.querySelector("input");
 let btn = document.querySelector("button");
 
-// var arr = [
-//     'elem 1',
-//     'elem 2',
-//     'elem 3',
-// ]
+var arr = []
 
-// arr.forEach((elem) => {
-//     let todoElem = document.createElement('li');
-//     let linkDelete = document.createElement('a')
-//     let nameDelete = document.createTextNode('delete')
-//     let todoText = document.createTextNode(elem);
+function render() {
 
-//     linkDelete.appendChild(nameDelete)
-//     todoElem.appendChild(todoText);
-//     todoElem.appendChild(linkDelete)
-//     list.appendChild(todoElem);
-// })
+    list.innerHTML = '';
+
+    arr.forEach((elem, index) => {
+        let todoElem = document.createElement('li');
+        let linkDelete = document.createElement('a')
+        linkDelete.setAttribute('href', '#')
+        // linkDelete.setAttribute('onclick', deleteItem(`${index}`))
+        let nameDelete = document.createTextNode('delete')
+        let todoText = document.createTextNode(elem);
+        
+        linkDelete.appendChild(nameDelete)
+        todoElem.appendChild(todoText);
+        todoElem.appendChild(linkDelete)
+        list.appendChild(todoElem);
+    })
+
+    input.value = ''
+}
+
+function deleteItem(pos) {
+    console.log(pos)
+    arr.splice(pos, 1);a
+    render()
+}
 
 btn.addEventListener("click", addElem);
 
 function addElem() {
     if (input.value.trim() != '') {
+        arr.push(input.value)
         render()
     }
 }
 
 document.addEventListener("keypress", (event) => {
   if (event.key === "Enter" && input.value.trim() != '') {
+    arr.push(input.value)
     render()
   }
 });
 
-function render() {
-    let createElem = document.createElement('li');
-    let delBtn = document.createElement('a');
-    delBtn.setAttribute('href', '#')
-    // delBtn.setAttribute('onclick', deleteItem())
+// function render() {
+//     let createElem = document.createElement('li');
+//     let delBtn = document.createElement('a');
+//     delBtn.setAttribute('href', '#')
+//     delBtn.setAttribute('onclick', deleteItem)
 
-    let nodeBtnDel = document.createTextNode('delete')
-    delBtn.appendChild(nodeBtnDel)
+//     let nodeBtnDel = document.createTextNode('delete')
+//     delBtn.appendChild(nodeBtnDel)
 
-    let nodeValue = document.createTextNode(input.value);
-    createElem.appendChild(nodeValue);
-    createElem.appendChild(delBtn)
+//     // console.log(delBtn)
+
+//     let nodeValue = document.createTextNode(input.value);
+//     createElem.appendChild(nodeValue);
+//     createElem.appendChild(delBtn)
   
-    list.appendChild(createElem);
-}
-
-// function deleteItem(pos) {
-//     list.splice(pos, 1);
+//     list.appendChild(createElem);
+//     console.log(list)
 // }
+
+
+
 
