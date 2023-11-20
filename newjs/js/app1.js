ac5e75cdbc2607dc483ae459985acd620a1c2d55
estevo <pedro.cordova2002@gmail.com>
2023-11-19 22:32:13 -0300

    axios implemented!

--- newjs/js/app1.js
+++ newjs/js/app1.js
@@ -40,27 +40,33 @@ const buttons = document.querySelectorAll('.accordion-button').forEach( button =
 })
 
 
-let myPromise = () => {
-    return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest();
+// let myPromise = () => {
+//     return new Promise((resolve, reject) => {
+//         let xhr = new XMLHttpRequest();
         
-        xhr.open('GET', 'https://api.github.com/users/nyousefali')
-        xhr.send(null)
+//         xhr.open('GET', 'https://api.github.com/users/nyousefali')
+//         xhr.send(null)
         
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText))
-                } else {
-                    reject('Error')
-                }
-            }
-        }
+//         xhr.onreadystatechange = () => {
+//             if (xhr.readyState === 4) {
+//                 if (xhr.status === 200) {
+//                     resolve(JSON.parse(xhr.responseText))
+//                 } else {
+//                     reject('Error')
+//                 }
+//             }
+//         }
         
-    })
-}
+//     })
+// }
 
-let result = myPromise()
-result.then((data) => {
-    console.log(data)
+// let result = myPromise()
+// result.then((data) => {
+//     console.log(data)
+// }).catch(err => alert(err))
+
+axios.get('https://api.github.com/users/nyousefali')
+.then((response) => {
+    console.log(response.data)
 }).catch(err => alert(err))
+
